de1074124455876cb8838a6787dd3900c0a2debc
kosenin@yandex.ru <L61nm1q2e4P8>
2018-03-29 11:14:24 +0300

    Fixed division by zero issue

--- src/HomeTask1.java
+++ src/HomeTask1.java
@@ -6,21 +6,41 @@ public class HomeTask1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Введите целое число:");
+        System.out.println("Введите два целых числа:");
 
         try {
             int integerOne = scanner.nextInt();
             int integerTwo = scanner.nextInt();
 
-            System.out.println("Сложение: " + (integerOne + integerTwo));
-            System.out.println("Вычитание: " + (integerOne - integerTwo));
-            System.out.println("Умножение: " + integerOne * integerTwo);
-            System.out.println("Деление: " + integerOne / integerTwo);
-            System.out.println("Остаток от деления: " + integerOne % integerTwo);
-
-        } catch (InputMismatchException i) {
-            System.out.println("Ошибка. Перезапустите программу и введите целое число");
+            multiplication(integerOne, integerTwo);
+            composition(integerOne, integerTwo);
+            division(integerOne, integerTwo);
+            deduction(integerOne, integerTwo);
+        } catch (Exception e) {
+            System.out.println("И все же числа должны быть целыми, напр. 2 или 3.");
         }
 
+
     }
+
+    private static void multiplication(int one, int two) {
+        System.out.println("Умножение: " + one * two);
+    }
+
+    private static void composition(int one, int two) {
+        System.out.println("Сложение: " + (one + two));
+    }
+
+    private static void division(int one, int two) {
+        if (two != 0) {
+            System.out.println("Деление: " + one / two);
+        } else
+            System.out.println("Нельзя делить на 0");
+    }
+
+    private static void deduction(int one, int two) {
+        System.out.println("Вычитание: " + (one - two));
+    }
+
+
 }
